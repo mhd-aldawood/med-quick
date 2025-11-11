b922bd786baffd7608e1a89499e2fff0b5e199b6
Mohamad Aldawood <dawood939m@gmail.com>
2025-11-11 13:21:26 +0400

    adding new device with state issue fix

--- app/src/main/java/com/example/kotlintest/core/DeviceManager.kt
+++ app/src/main/java/com/example/kotlintest/core/DeviceManager.kt
@@ -1,28 +1,31 @@
 package com.example.kotlintest.core
 
-import com.example.kotlintest.core.bluetooth.BluetoothRepositoryImpl
+import com.example.kotlintest.core.bluetooth.BluetoothScanner
 import com.example.kotlintest.core.model.ConnectionState
 import com.example.kotlintest.screens.home.DeviceCategory
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 data class DeviceConfigure(
-    val connectionState: ConnectionState,
+    val connectionState: ConnectionState = ConnectionState.Idle,
     var deviceCategory: DeviceCategory? = null
 )
 
+class DeviceManager @Inject constructor(val bluetoothScanner: BluetoothScanner) {
+    private val _deviceConfigure = MutableStateFlow(DeviceConfigure())
+    val deviceConfigure: StateFlow<DeviceConfigure> = _deviceConfigure
 
-class DeviceManager @Inject constructor(val bluetoothRepositoryImpl: BluetoothRepositoryImpl) {
-    val deviceConfigure: DeviceConfigure =
-        DeviceConfigure(
-            connectionState = ConnectionState.Idle,
-        )
+    fun setConnectionState(state: ConnectionState) {
+        _deviceConfigure.update { it.copy(connectionState = state) }
+    }
     private var deviceModels: List<String>? = null
 
     fun getDeviceModels(): List<String> = deviceModels!!
 
 
     fun setDeviceType(deviceCategory: DeviceCategory) {
-        deviceConfigure.deviceCategory = deviceCategory
         setDeviceModels(deviceCategory)
     }
 
@@ -30,7 +33,11 @@ class DeviceManager @Inject constructor(val bluetoothRepositoryImpl: BluetoothRe
         deviceModels = when (deviceCategory) {
             DeviceCategory.DigitalStethoscope -> listOf("Mintti")
             DeviceCategory.DopplerUltrasound -> TODO()
-            DeviceCategory.ECGWorkstation -> TODO()
+            DeviceCategory.ECGWorkstation -> listOf<String>(
+                "ECGWS",
+                "8000GW",
+                "CB100",
+            )
             DeviceCategory.ElectronicSphygmomanometer -> listOf(
                 "NIBP01",
                 "NIBP03",
@@ -65,6 +72,14 @@ class DeviceManager @Inject constructor(val bluetoothRepositoryImpl: BluetoothRe
 
 
     }
+
+    /*
+    *
+    *
+    * THE PURPOSE FROM THIS METHOD IS TO CHECK IF I AM STILL CONNECTED TO THE DEVICE AND THE STATUS OR NOT
+    * */
+    public fun isConnected(): Boolean =
+        deviceConfigure.value.connectionState == ConnectionState.Connected
 }
 
 interface DeviceOperation {
