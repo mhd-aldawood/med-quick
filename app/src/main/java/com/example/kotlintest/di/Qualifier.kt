77612e536b5bf7b699e35e235063a6f3e8188a79
Mohamad Aldawood <dawood939m@gmail.com>
2025-11-11 13:17:09 +0400

    missing qualifier

---
+++ app/src/main/java/com/example/kotlintest/di/Qualifier.kt
@@ -0,0 +1,20 @@
+package com.example.kotlintest.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class TonometerQualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class StethoScopeQualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class PulseOximeterQualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ThermometerQualifier
+
